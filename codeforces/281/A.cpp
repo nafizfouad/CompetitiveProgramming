b459d764d1e4a86fe520bfad51403537463bd165
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2019-12-18 20:30:00 +0600

    Add solution for problem `A - Word Capitalization` Link: https://codeforces.com/contest/281/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/281/submission/67162768

---
+++ codeforces/281/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    int i,j,k,a,b,c;
+    string s,v;
+    cin>>s;
+    if(s[0]>='a' && s[0]<='z'){
+        s[0] = s[0]-32;
+    }
+    cout<<s<<endl;
+    s.clear();
+    return 0;
+}
