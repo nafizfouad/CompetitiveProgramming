be74a3e1d20c6875f881a17105080ee1e8c04318
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2019-12-28 23:12:00 +0600

    Add solution for problem `A - Minutes Before the New Year` Link: https://codeforces.com/contest/1283/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1283/submission/67803013

---
+++ codeforces/1283/A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+int i,j,k,a,b,c,h,m,t,n;
+cin>>t;
+for(i=1;i<=t;i++){
+    cin>>h>>m;
+a = 23-h;
+b = 60-m;
+c = a*60;
+c = b+c;
+cout<<c<<endl;
+}
+return 0;
+}
