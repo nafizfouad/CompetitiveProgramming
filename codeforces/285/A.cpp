2de17596497194ba1567e50fe4d23866adbf4b1c
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-02-14 00:02:00 +0600

    Add solution for problem `A - Slightly Decreasing Permutations` Link: https://codeforces.com/contest/285/problem/A Tags: greedy, implementation, *1100 Ref: https://codeforces.com/contest/285/submission/71013570

---
+++ codeforces/285/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i,j,k,a,b,c,d,e;
+    cin>>a>>b;
+    for(i=a,j=1;j<=b;j++,i--){
+        
+        cout<<i<<" ";
+    }
+    for(i=1;i<=(a-b);i++){
+    cout<<i<<" ";
+}
+    cout<<endl;
+}
