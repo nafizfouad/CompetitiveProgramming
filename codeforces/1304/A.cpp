d3d1b9aad1a1d2c34d1747e6827dd577259561ec
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-02-16 00:14:00 +0600

    Add solution for problem `A - Two Rabbits` Link: https://codeforces.com/contest/1304/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1304/submission/71176816

---
+++ codeforces/1304/A.cpp
@@ -0,0 +1,4 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){ll i,j,k,a,b,c,d,n,t,l,f,x,y; vector<ll> v,s; vector<ll>::iterator it,ki;map<pair<ll,ll>,ll>ma,sa;string st; cin>>t;for(i=1;i<=t;i++) {cin>>x>>y>>a>>b;c=a+b;d=y-x;if(d%c==0){cout<<d/c<<endl;}else cout<<-1<<endl;}}
