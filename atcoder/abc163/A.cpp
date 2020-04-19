2042171603369fe5dff9688af35b2a3527ba7a6e
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-04-19 18:08:00 +0600

    Add solution for problem `A - Circle Pond` Link: https://atcoder.jp/contests/abc163/tasks/abc163_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc163/submissions/12098633

---
+++ atcoder/abc163/A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+#include<iomanip>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+
+    //ll i,j,k,a,b,c,n,t,m,d,r,g,y,f;
+   // string st,et,mt;
+    long double aa,bb;
+    cin>>aa;
+    bb = acos(-1);
+    cout<<setprecision(20)<<fixed<<bb*aa*2*1.0<<endl;
+}
