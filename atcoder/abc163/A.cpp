9ae93fcd69d8b26ef3203f7552e8349d5a4a3adb
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-04-19 18:03:00 +0600

    Add solution for problem `A - Circle Pond` Link: https://atcoder.jp/contests/abc163/tasks/abc163_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc163/submissions/12081266

--- atcoder/abc163/A.cpp
+++ atcoder/abc163/A.cpp
@@ -1,14 +1,48 @@
-#include<iostream>
-#include<math.h>
-#include<iomanip>
+#include<bits/stdc++.h>
 #define ll long long int
 using namespace std;
+map<ll,ll > ma;
+vector<ll> ki;
+vector<pair<ll,ll> > it;
+void sieve()
+{
+    ma[1] = 1;
+    int i;;
+    for( i=3; i*i<=20000010; i=i+2)
+    {
+        if(ma[i]==0)
+        {
+            for(int j=i*i; j<=20000010; j=j+(2*i))
+            {
+                ma[j] = 1;
+            }
+        }
+
+    }
+    int c1,c2;
+    c1 = 3;
+    c2 = 3;
+    for(i=5; i<=20000010; i=i+2)
+    {
+        if(ma[i]==0)
+        {
+            c1=c2;
+            c2=i;
+            if((c2-c1)==2)
+            {
+                it.push_back({c1,c2});
+            }
+        }
+    }
+}
 
 int main()
 {
 
-    //ll i,j,k,a,b,c,n,t,m,d,r,g,y,f;
-   // string st,et,mt;
+    ll i,j,k,a,b,c,n,t,m,d,r,g,y,f;
+    vector<ll> v,s,ss,vv;
+    map<char,ll> ga,ha;
+    string st,et,mt;
     long double aa,bb;
     cin>>aa;
     bb = acos(-1);
