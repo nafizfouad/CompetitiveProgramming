b29e8a486489646976a58c73f7590853a54d2ed5
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-05-15 01:42:00 +0600

    Add solution for problem `A - Again Twenty Five!` Link: https://codeforces.com/contest/630/problem/A Tags: number theory, *800 Ref: https://codeforces.com/contest/630/submission/80170851

---
+++ codeforces/630/A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll int
+using namespace std;
+int main()
+{
+    ll i,j,k,a,b,c,n,t,m,p,q,d,e,f;
+    vector<ll > v,s(100010,0),vv,ss;
+    map<ll,ll> ma,sa;
+    string st,et,ft,pt,ct;
+    deque<char> de;
+    ios_base::sync_with_stdio(false);
+    cin>>n;
+    cout<<25<<endl;
+}
+
+
