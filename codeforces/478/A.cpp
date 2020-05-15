e80ce2d63a9cbc97435cfe27d77a9ebe8ab742a2
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-05-15 06:15:00 +0600

    Add solution for problem `A - Initial Bet` Link: https://codeforces.com/contest/478/problem/A Tags: implementation, *1100 Ref: https://codeforces.com/contest/478/submission/80180784

---
+++ codeforces/478/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ll i,j,k,a,b,c,n,t,m,p,q,d,e,f,l,nl,np;
+    deque<ll> v,s,vv,ss;
+    map<ll,ll> ma,sa;
+    string st,et,ft,pt,ct;
+    deque<char> de;
+    ios_base::sync_with_stdio(false);
+    cin>>a>>b>>c>>d>>e;
+    a = a+b+c+d+e;
+    if(a%5==0 && a!=0)cout<<a/5<<endl;
+    else cout<<-1<<endl;
+}
+
+
+
