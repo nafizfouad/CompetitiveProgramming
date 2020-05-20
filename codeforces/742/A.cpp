6187aff69ff325ef265ad275cfa61caf57cfe7e0
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-05-20 15:52:00 +0600

    Add solution for problem `A - Arpa’s hard exam and Mehrdad’s naive cheat` Link: https://codeforces.com/contest/742/problem/A Tags: implementation, math, number theory, *1000 Ref: https://codeforces.com/contest/742/submission/80760198

---
+++ codeforces/742/A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ll i,j,k,a,b,c,n,t,m,p,q,d,e,f,l,nl,np;
+    vector<ll> v,s,vv,ss;
+    map<ll,ll> ma,sa;
+    string st,et,ft,pt,ct;
+    deque<char> de;
+    ios_base::sync_with_stdio(false);
+    cin>>n;
+    if(n==0)cout<<1<<endl;
+   else if(n%4==0)cout<<6<<endl;
+   else if(n%4==1)cout<<8<<endl;
+   else if(n%4==2)cout<<4<<endl;
+   else if(n%4==3)cout<<2<<endl;
+}
