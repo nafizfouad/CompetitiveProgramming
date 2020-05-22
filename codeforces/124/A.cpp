4e127b481c2687f6c96f78a8e363f96b6a7a8909
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-05-22 20:00:00 +0600

    Add solution for problem `A - The number of positions` Link: https://codeforces.com/contest/124/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/124/submission/80939648

---
+++ codeforces/124/A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ll i,j,k,a,b,c,n,t,m,p,q,d,e,f,l,nl,np;
+    // vector<pair<ll,ll> > v;
+    vector<ll> s,vv,ss,v;
+    string st,et,ft,pt,ct;
+    deque<char> de;
+    ios_base::sync_with_stdio(false);
+    cin>>n>>a>>b;
+    if(a+b<n)cout<<b+1<<endl;
+    else if(a+b==n)cout<<b<<endl;
+    else cout<<n-a<<endl;
+}
