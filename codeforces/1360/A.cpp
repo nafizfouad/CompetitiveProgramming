9cc7495911b450ec1aa9b99e1f73ca1de700ef60
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-05-24 19:43:00 +0600

    Add solution for problem `A - Minimal Square` Link: https://codeforces.com/contest/1360/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1360/submission/81210643

---
+++ codeforces/1360/A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ll i,j,k,a,b,c,n,t,m,p,q,d,e,f,l,nl,np;
+    // vector<pair<ll,ll> > v;
+    vector<ll > s,vv,ss,v;
+    map<string,ll> ma,sa;
+    string st,et,ft,pt,ct;
+    deque<char> de;
+    ios_base::sync_with_stdio(false);
+    cin>>n;
+    c=0;
+    d = 0;
+    for(i=0; i<n; i++)
+    {
+        cin>>a>>b;
+        c = min(a,b);
+        c = c*2;
+        if(c<max(a,b)){
+            cout<<max(a,b)*max(a,b)<<endl;
+        }
+        else {
+          c = c*c;
+        cout<<c<<endl;
+        }
+
+    }
+}
