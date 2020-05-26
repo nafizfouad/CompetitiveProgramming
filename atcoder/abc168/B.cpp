ca098db08675a6a4e9185f837940e4ed233f9069
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-05-26 23:58:00 +0600

    Add solution for problem `B - ... (Triple Dots)` Link: https://atcoder.jp/contests/abc168/tasks/abc168_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc168/submissions/13628098

---
+++ atcoder/abc168/B.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ll i,j,k,a,b,c,n,t,m,p,q,d,e,f,l,nl,np,x,y,x1,y2,y1,x2;
+    vector<ll > s;
+    vector<ll > vv,ss,v;
+    map<string,ll> ma,sa;
+    string st,et,ft,pt,ct;
+    deque<char> de;
+    ios_base::sync_with_stdio(false);
+    cin>>a;
+    cin>>st;
+    if(st.size()<=a)cout<<st<<endl;
+    else {
+        for(i=0;i<a;i++)cout<<st[i];
+        cout<<"..."<<endl;
+    }
+}
