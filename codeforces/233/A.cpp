37e6e3efce9ebfdd7f357c81c6d9f79c6871db05
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-05-29 00:11:00 +0600

    Add solution for problem `A - Perfect Permutation` Link: https://codeforces.com/contest/233/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/233/submission/81818731

---
+++ codeforces/233/A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+
+    ll i,j,k,c,d,a,b,e,f,p,q,n,t,m;
+    vector<ll > vv,ss,v,s;
+    string st,et;
+    map<ll,ll> pa,ea;
+    cin>>n;
+    if(n%2==1)cout<<-1<<endl;
+    else{
+      for(i=n;i>=1;i--){
+        cout<<i<<" ";
+    }
+    cout<<endl;  
+    }
+    
+}
