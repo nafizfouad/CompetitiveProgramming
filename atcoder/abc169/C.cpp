a4c8697bfe0cf37a860a15db449a5bc558e70c0b
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-05-31 18:18:00 +0600

    Add solution for problem `C - Multiplication 3` Link: https://atcoder.jp/contests/abc169/tasks/abc169_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc169/submissions/13813622

---
+++ atcoder/abc169/C.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+
+    ll i,j,k,c,d,a,b,e,f,p,q,n,t,m,r,u,x,y;
+    vector<ll > vv,v,s;
+    string st,et;
+    map<ll,ll > pa,ea,ma;
+    long double bb,cc,aa;
+    cin>>a>>bb;
+    aa = a*bb;
+    c = aa/1;
+    cout<<c<<endl;
+}
