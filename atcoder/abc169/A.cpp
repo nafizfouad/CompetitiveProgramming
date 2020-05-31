bf19826d477d7e363649fbab9adb0d6bf0f7ff26
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-05-31 18:00:00 +0600

    Add solution for problem `A - Multiplication 1` Link: https://atcoder.jp/contests/abc169/tasks/abc169_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc169/submissions/13776992

---
+++ atcoder/abc169/A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+
+    ll i,j,k,c,d,a,b,e,f,p,q,n,t,m,r,u,x,y;
+    vector<ll > vv,ss,v,s;
+    string st,et;
+    map<ll,ll > pa,ea,ma;
+    cin>>a>>b;
+    cout<<a*b<<endl;
+}
