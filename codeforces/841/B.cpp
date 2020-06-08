40ecaba7fb2c5004eeffbea64d2f5a79901fbe58
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-06-09 03:09:00 +0600

    Add solution for problem `B - Godsend` Link: https://codeforces.com/contest/841/problem/B Tags: games, math, *1100 Ref: https://codeforces.com/contest/841/submission/83172184

---
+++ codeforces/841/B.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define fo(i0n) for(i=0;i<n;i++)
+#define fo(i1n) for(i=1;i<=n;i++)
+#define fo(j0n) for(j=0;j<n;j++)
+#define fo(j1n) for(j=1;j<=n;j++)
+#define fast() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pb push_back
+#define pf push_front
+#define pu push
+#define bg begin
+#define en end
+using namespace std;
+int main()
+{
+    fast();
+    ll i,j,k,c,d,a,b,e,f,p,q,n,t,m,r,u,g,x,y,l;
+    ld bb,cc,aa;
+    string st,et,ft;
+    priority_queue<ll > pq,aq,dq;
+    map<string,string> ma,ta;
+    map<string,ll> sa;
+    map<ll,ll> pa;
+    set<ll> se,pe,me;
+    vector<pair<string,string> >vv,v,s;
+
+    d = 0;
+    cin>>n;
+
+    for(i=0; i<n; i++)
+    {
+        cin>>a;
+        if(a%2==1)d=1;
+    }
+    if(d==1)cout<<"First"<<endl;
+    else{
+        cout<<"Second"<<endl;
+    }
+}
