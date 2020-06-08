c3e1fdba6789ac00102a312d2cd9c34973af2265
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-06-09 03:57:00 +0600

    Add solution for problem `B - Knights` Link: https://codeforces.com/contest/1221/problem/B Tags: constructive algorithms, greedy, *1100 Ref: https://codeforces.com/contest/1221/submission/83173498

---
+++ codeforces/1221/B.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define fo(i0n) for(i=0;i<n;i++)
+#define fo(i1n) for(i=1;i<=n;i++)
+#define fo(j0n) for(j=0;j<n;j++)
+#define fo(j1n) for(j=1;j<=n;j++)
+#define fast() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pb push_back
+#define pf push_front
+#define pu push
+#define bg begin
+#define en end
+using namespace std;
+int main()
+{
+    fast();
+    ll i,j,k,c,d,a,b,e,f,p,q,n,t,m,r,u,g,x,y,l;
+    ld bb,cc,aa;
+    string st,et,ft;
+    priority_queue<ll > pq,aq,dq;
+    map<ll,ll> ma,ta;
+    map<string,ll> sa;
+    map<ll,ll> pa;
+    set<ll> se,pe,me;
+    vector<pair<string,string> >vv,v,s;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            if(i%2==0){
+                if(j%2==0)cout<<"W";
+                else cout<<"B";
+            }
+            else{
+                if(j%2==1)cout<<"W";
+                else cout<<"B";
+            }
+        }
+        cout<<endl;
+    }
+}
