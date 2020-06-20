734f4b6781dfb84834f0e7feddce8e9a126210e5
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-06-20 20:38:00 +0600

    Add solution for problem `A - Maximum GCD` Link: https://codeforces.com/contest/1370/problem/A Tags: greedy, implementation, math, number theory, *800 Ref: https://codeforces.com/contest/1370/submission/84427553

---
+++ codeforces/1370/A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+using namespace std;
+int main()
+{
+    ll i,j,k,a,b,c,n,t,m,p,q,d,e,f,x,y,g,z,h;
+    ld aa,bb,cc;
+    vector<ll > v,s,vv;
+    map<ll,string> sa,ta,ma;
+    priority_queue<ll > aq,tq,pq;
+    string st,et,ft,pt,ct,ss;
+    deque<ll> de;
+    ios_base::sync_with_stdio(false);
+    cin>>n;
+    for(i=0;i<n;i++){
+        cin>>a;
+        if(a%2==1)a--;
+        cout<<a/2<<endl;
+    }
+}
+
