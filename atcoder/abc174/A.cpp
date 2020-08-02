685727c339432da07324f3acd9f31184037e02d9
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-08-02 18:01:00 +0600

    Add solution for problem `A - Air Conditioner` Link: https://atcoder.jp/contests/abc174/tasks/abc174_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc174/submissions/15592617

---
+++ atcoder/abc174/A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+using namespace std;
+
+int main()
+{
+    ll i,j,a,k,b,c,n,t,m,p,q,d,e,f,y,g,z,h,r,l,x;
+    ld aa,bb,ee,dd,ff,kk,gg;
+    
+    ios_base::sync_with_stdio(false);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+        cin>>n;
+        if(n>=30)cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+}
