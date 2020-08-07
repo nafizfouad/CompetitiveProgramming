bc2385236d2fc3e84a8697abc4649caebe010bd5
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-08-07 21:05:00 +0600

    Add solution for problem `A - Rainbow Dash, Fluttershy and Chess Coloring` Link: https://codeforces.com/contest/1393/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1393/submission/89232777

---
+++ codeforces/1393/A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+using namespace std;
+
+int main()
+{
+    ll i,j,a,k,b,c,n,t,m,p,q,d,e,f,y,g,z,h,r,l,x;
+    ld aa,bb,ee,dd,ff,kk,gg;
+    vector<ll > v,s;
+    vector<string> ss,vv,pp;
+    map<ll, ll > ta,pa,ma,sa;
+    map<pair< deque<ll>,deque<ll> >,ll  > me;
+    priority_queue<ll > aq,tq,pq;
+    string st,et,ft,pt,ct;
+    deque<ll> de,ne;
+    ios_base::sync_with_stdio(false);
+    ll ar[501][501];
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+        cin>>n;
+        for(i=0; i<n; i++)
+        {
+            cin>>a;
+            b = a/2;
+            cout<<b+1<<endl;
+        }
+
+}
