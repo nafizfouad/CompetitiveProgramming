ebef9a1a2d7b7edd79aa9e65dd56ced01e3467b3
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-09-13 18:02:00 +0600

    Add solution for problem `A - Not` Link: https://atcoder.jp/contests/abc178/tasks/abc178_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc178/submissions/16677334

---
+++ atcoder/abc178/A.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<deque>
+#include<queue>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+using namespace std;
+
+int main()
+{
+    ll i,j,a,k,b,c,n,t,m,p,q,d,e,f,y,g,z,h,r,l,x,o;
+    ld aa,bb,cc,ee,dd,ff,kk,gg;
+    vector<ll > v,s;
+    map<ll,vector<ll> > me;
+    map<ll,ll > sa,ta,pa,ma;
+    priority_queue<ll, vector<ll>, greater<ll> > aq,tq,pq;
+    string st,et,ft,pt,ct;
+    deque<ll> de,pe,ne;
+    ios_base::sync_with_stdio(false);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+    cin>>n;
+    if(n==0)cout<<1<<endl;
+    else if(n==1)cout<<0<<endl;
+}
