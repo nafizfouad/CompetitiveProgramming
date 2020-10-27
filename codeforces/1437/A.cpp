58d6ad2b9bcc19b12fe0be09867265f7f34855d5
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-10-27 20:43:00 +0600

    Add solution for problem `A - Marketing Scheme` Link: https://codeforces.com/contest/1437/problem/A Tags: brute force, constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1437/submission/96871768

---
+++ codeforces/1437/A.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+using namespace std;
+
+
+int main()
+{
+    ll i,j,a,k,b,c,n,t,m,p,q,d,e,f,y,g,z,h,r,l,x,o;
+    ld aa,bb,ee,dd,ff,kk,gg;
+    vector< ll > v,s,vv;
+    map<ll,vector<ll> > me;
+    map<ll ,ll > sa,ta,pa,ma;
+    priority_queue<ll, vector<ll>, greater<ll> > aq,tq,pq;
+    string st,et,ft,pt,ct;
+    deque<char> de,pe,ne;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        c = b+1;
+        d = a%c;
+        aa = (c*1.0)/2.0;
+        if(d>=aa)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
+
+
