1b6be2218f99b4883485e1ba80107fb3dd0d1ffc
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-11-02 02:58:00 +0600

    Add solution for problem `B - Elimination` Link: https://codeforces.com/contest/1445/problem/B Tags: greedy, math, *900 Ref: https://codeforces.com/contest/1445/submission/97385761

---
+++ codeforces/1445/B.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<deque>
+#include<queue>
+#include<string.h>
+#include<stdio.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+using namespace std;
+
+int main()
+{
+    ll i,j,a,k,b,c,n,t,m,p,q,d,e,f,y,g,z,h,r,l,x,o;
+    ld aa,bb,ee,dd,ff,kk,gg;
+    vector< ll > v,s,vv;
+    vector< pair<ll,ll> > ss;
+    map<ll,vector<ll> > me;
+    map<ll,ll > sa,ta,pa,ma;
+    priority_queue<ll, vector<ll>, greater<ll> > aq,tq,pq;
+    string st,et,ft,pt,ct;
+    deque<ll> de,pe,ne;
+    ios_base::sync_with_stdio(false);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    cin>>t;
+    for(i=0; i<t; i++)
+    {
+        cin>>a>>b>>c>>d;
+        m = max(a+b,c+d);
+        cout<<m<<endl;
+    }
+
+}
