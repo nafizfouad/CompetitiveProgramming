029a8789146f16f65d0796e080084acd559bafc2
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-11-02 02:50:00 +0600

    Add solution for problem `A - Array Rearrangment` Link: https://codeforces.com/contest/1445/problem/A Tags: greedy, sortings, *800 Ref: https://codeforces.com/contest/1445/submission/97385506

---
+++ codeforces/1445/A.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<deque>
+#include<queue>
+#include<string.h>
+#include<stdio.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+using namespace std;
+
+int main()
+{
+    ll i,j,a,k,b,c,n,t,m,p,q,d,e,f,y,g,z,h,r,l,x,o;
+    ld aa,bb,ee,dd,ff,kk,gg;
+    vector< ll > v,s,vv;
+    vector< pair<ll,ll> > ss;
+    map<ll,vector<ll> > me;
+    map<ll,ll > sa,ta,pa,ma;
+    priority_queue<ll, vector<ll>, greater<ll> > aq,tq,pq;
+    string st,et,ft,pt,ct;
+    deque<ll> de,pe,ne;
+    ios_base::sync_with_stdio(false);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    cin>>t;
+    for(i=0; i<t; i++)
+    {
+        cin>>n>>x;
+        v.clear();
+        s.clear();
+        ss.clear();
+        for(j=0;j<n;j++){
+            cin>>a;
+            v.pb(a);
+        }
+        for(j=0;j<n;j++){
+            cin>>a;
+            s.pb(a);
+        }
+        sort(v.begin(),v.end());
+        f = 0;
+        sort(s.begin(),s.end(),greater<ll>());
+        for(j=0;j<n;j++){
+            if(v[j]+s[j]<=x)ss.pb({v[j],s[j]});
+            else {f=1;break;}
+        }
+        if(f==1)cout<<"No"<<endl;
+        else{
+            cout<<"Yes"<<endl;
+        }
+    }
+
+}
