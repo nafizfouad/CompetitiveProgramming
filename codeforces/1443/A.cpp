874d133349f447d0f28cd3520a9852dd45002114
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-11-02 20:41:00 +0600

    Add solution for problem `A - Kids Seating` Link: https://codeforces.com/contest/1443/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1443/submission/97437517

---
+++ codeforces/1443/A.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+using namespace std;
+
+
+int main()
+{
+    ll i,j,a,k,b,c,n,t,m,p,q,d,e,f,y,g,z,h,r,l,x,o;
+    ld aa,bb,ee,dd,ff,kk,gg;
+    vector< ll > v,s,vv;
+    map<ll,pair<ll,ll> > me;
+    map<ll ,ll > sa,ta,pa,ma;
+    priority_queue<ll, vector<ll>, greater<ll> > aq,tq,pq;
+    string st,et,ft,pt,ct;
+    deque<char> de,pe,ne;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    cin>>n;
+    for(i=0;i<n;i++){
+        cin>>a;
+        b= a;
+        a = a*2;
+        for(j=a,k=1;k<=b;j=j+2,k++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+    /// moulik songkha diye n ke bhag diye dekhte hobe k er cheye <= kina
+}
+
+
+
