047a9b47a6a79b19c413420545bf748f77a647e8
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-11-13 20:37:00 +0600

    Add solution for problem `A - Specific Tastes of Andre ` Link: https://codeforces.com/contest/1438/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/1438/submission/98282846

---
+++ codeforces/1438/A.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+using namespace std;
+
+int main()
+{
+    ll i,j,a,k,b,c,n,t,m,p,q,d,e,f,y,g,z,h,r,l,x;
+    ld aa,bb,ee,dd,ff,cc,rr,kk,gg;
+    vector<ll > v,s;
+    vector<pair<ll,ll> > ss,pp;
+    map<char,ll > mm;
+    map<pair<ld, ld>,ll>  sa,ta,ma,pa;
+    ios_base::sync_with_stdio(false);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    cin>>n;
+    for(i=1;i<=n;i++){
+        cin>>a;
+        for(j=1;j<=a;j++){
+            cout<<1<<" ";
+        }
+        cout<<endl;
+    }
+
+}
