6b10919600586ff55ba5ff508493ca1a3afb1d79
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-11-15 20:50:00 +0600

    Add solution for problem `A - Add Candies` Link: https://codeforces.com/contest/1447/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1447/submission/98452299

---
+++ codeforces/1447/A.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<map>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+using namespace std;
+
+int main()
+{
+    ll i,j,a,k,b,c,n,t,m,p,q,d,e,f,y,g,z,h,r,l,x;
+    ld aa,bb,ee,dd,ff,cc,rr,kk,gg;
+    vector<ll > v,s;
+    vector<string > ss,pp;
+    map<string,ll > mm;
+    map<pair<ll, char>,ll>  sa,ta,ma,pa;
+    string st,et,ft;
+    ios_base::sync_with_stdio(false);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        cout<<n<<endl;
+        for(i=1;i<=n;i++){
+          cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
