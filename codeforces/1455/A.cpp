dad5deaf3ad5869834d81958cdcf3d02aa4c6309
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-11-30 22:17:00 +0600

    Add solution for problem `A - Strange Functions` Link: https://codeforces.com/contest/1455/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1455/submission/100044678

---
+++ codeforces/1455/A.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+using namespace std;
+
+int main()
+{
+    ll i,j,a,k,b,c,n,t,m,p,q,d,e,f,y,g,z,h,r,l,x,o;
+    ld aa,bb,cc,ee,dd,ff,kk,gg;
+    vector<ll > v,s,vv;
+    vector<ll> ss,pp;
+    map<ll,vector<ll> > me;
+    map<ll,ll > ta,pa,ma,sa;
+    priority_queue<ll, vector<ll>, greater<ll> > aq,tq,pq;
+    string st,et,ft,pt,ct;
+    deque<ll> de,pe,ne;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    cin>>t;
+    while(t--)
+    {
+        cin>>st;
+        cout<<st.size()<<endl;
+    }
+//    cin>>t;
+//    while(t--)
+//    {
+//        cin>>n>>k;
+//        v.clear();
+//        for(i=0; i<n; i++)
+//        {
+//            cin>>a;
+//            v.pb(a);
+//        }
+//        if(is_sorted(v.begin(),v.end()))cout<<0<<endl;
+//        else
+//        {
+//            c = 0;
+//            for(i=0; i+1<n; i++)
+//            {
+//                if(v[i]>k)
+//                {
+//                    swap(v[i],k);
+//                    c++;
+//                }
+//            }
+//             if(is_sorted(v.begin(),v.end()))cout<<c<<endl;
+//             else cout<<-1<<endl;
+//        }
+//    }
+}
