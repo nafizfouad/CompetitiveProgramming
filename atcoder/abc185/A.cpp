7ff32368dce0a57902435a4e2b31463c9ee41b9a
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-12-14 02:31:00 +0600

    Add solution for problem `A - ABC Preparation` Link: https://atcoder.jp/contests/abc185/tasks/abc185_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc185/submissions/18771952

---
+++ atcoder/abc185/A.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<deque>
+#include<queue>
+#include<set>
+#include<string.h>
+#include<stdio.h>
+#include<math.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+using namespace std;
+
+int main()
+{
+    ll i,j,a,k,b,c,n,t,m,p,q,d,e,f,y,g,z,h,r,l,x,o;
+    ld aa,bb,cc,ee,dd,ff,kk,gg;
+    vector< ll > v,s,vv;
+    map<ll,vector<ll> > me;
+    map<ll,ll > sa,ta,pa,ma;
+    priority_queue<ll, vector<ll>, greater<ll> > aq,tq,pq;
+    string st,et,ft,pt,ct;
+    deque<ll> de,pe,ne;
+    ios_base::sync_with_stdio(false);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    cin>>a>>b>>c>>d;
+    cout<<min(min(a,b),min(c,d))<<endl;
+}
