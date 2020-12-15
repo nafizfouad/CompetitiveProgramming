fafaff8dad2b370af81751c1ca053bdd81d7b4b4
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2020-12-15 20:40:00 +0600

    Add solution for problem `A - Favorite Sequence` Link: https://codeforces.com/contest/1462/problem/A Tags: implementation, two pointers, *800 Ref: https://codeforces.com/contest/1462/submission/101263818

---
+++ codeforces/1462/A.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+using namespace std;
+
+int main()
+{
+    ll i,j,a,k,b,c,n,t,e,m,p,q,d,f,y,g,z,h,r,l,x,o;
+    ld aa,bb,cc,ee,dd,ff,kk,gg,pp;
+    vector<ll > v,s,vv,tt;
+    vector<ll> ss;
+    map<pair<ll,ll>,ll > me;
+    map<ll,ll > ta,pa,ma,sa;
+    priority_queue<ll, vector<ll>, greater<ll> > aq,tq,pq;
+    string st,et,ft,pt,ct;
+    deque<ll> de,pe,ne;
+    set<ll> sett;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    cin>>t;
+    while(t--)
+    {
+     cin>>n;
+     for(i=0;i<n;i++){
+        cin>>a;
+        v.pb(a);
+     }
+     for(i=0,j=n-1;i<j;i++,j--){
+        cout<<v[i]<<" "<<v[j]<<" ";
+     }
+     if(n%2==1)cout<<v[n/2];
+     cout<<endl;
+     v.clear();
+    }
+
+}
