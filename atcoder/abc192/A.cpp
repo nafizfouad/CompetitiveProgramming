3261ff54b9ac793cda7455a32251987a5418089d
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-02-23 01:26:00 +0600

    Add solution for problem `A - Star` Link: https://atcoder.jp/contests/abc192/tasks/abc192_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc192/submissions/20428722

---
+++ atcoder/abc192/A.cpp
@@ -0,0 +1,39 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s;
+vector<pair<ll,ll> > pp;
+map<ll,char > ta,pa,sa,ma;
+map<ll ,string> me,he,mem;
+map<string,ll>se;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<pair<ll,ll> > li;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    cin>>n;
+    cout<<100-(n%100)<<endl;
+}
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
