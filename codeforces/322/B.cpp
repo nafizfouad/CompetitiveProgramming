e1b18fe3d7259eb297c9cbc2eb0f214627b5e4cc
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-04-11 22:00:00 +0600

    Add solution for problem `B - Ciel and Flowers` Link: https://codeforces.com/contest/322/problem/B Tags: combinatorics, math, *1600 Ref: https://codeforces.com/contest/322/submission/112698280

---
+++ codeforces/322/B.cpp
@@ -0,0 +1,55 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,vv;
+vector<ll> ss,pp;
+map<ll,ll > ta,pa,sa,ma;
+map<pair<ll,ll>,ll > me,he,mem;
+map<string,ll>se;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<char> de,ne;
+set<ll> li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+   // cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>a>>b>>c;
+        f = a/3+b/3+c/3;
+        d=-1e18;
+        e = -1e18;
+        if(a>=1 && b>=1 && c>=1)d = ((a-1)/3)+((b-1)/3)+((c-1)/3)+1;
+        if(a>=2 && b>=2 && c>=2)e = ((a-2)/3)+((b-2)/3)+((c-2)/3)+2;
+        f = max(f,d);
+        f = max(f,e);
+        cout<<f<<endl;
+    }
+}
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
