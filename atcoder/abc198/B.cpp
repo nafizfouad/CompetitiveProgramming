8bb7cfb4377b58d60febaab91a69567134a897e4
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-04-11 18:07:00 +0600

    Add solution for problem `B - Palindrome with leading zeros` Link: https://atcoder.jp/contests/abc198/tasks/abc198_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc198/submissions/21643435

---
+++ atcoder/abc198/B.cpp
@@ -0,0 +1,58 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,vv;
+vector<ll> ss,pp;
+map<ll,ll > ta,pa,sa,ma;
+map<pair<ll,ll>,ll > me,he,mem;
+map<string,ll>se;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<char> de,ne;
+set<ll> li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>st;
+        reverse(st.begin(),st.end());
+        i = 0;
+        if(st[i]=='0'){
+            while(st[i]=='0'){i++;if(i==st.size())break;}
+        }
+        f = 0;
+        for(k=i,j=st.size()-1;k<=j;k++,j--){
+            if(st[k]!=st[j]){f=1;break;}
+        }
+        if(f==0)cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
+}
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
