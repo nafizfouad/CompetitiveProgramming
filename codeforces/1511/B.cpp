091926e43304b73e95504e323a8924811f10621b
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-04-12 23:59:00 +0600

    Add solution for problem `B - GCD Length` Link: https://codeforces.com/contest/1511/problem/B Tags: constructive algorithms, math, number theory, *1100 Ref: https://codeforces.com/contest/1511/submission/112866607

---
+++ codeforces/1511/B.cpp
@@ -0,0 +1,62 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,vv;
+vector<ll> ss,pp;
+map<ll,ll > ta,pa,sa,ma;
+map<pair<ll,ll>,ll > me,he,mem;
+map<string,ll>se;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+unordered_multiset<ll> li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    cin>>t;
+    p = 0;
+    while(t--)
+    {
+       cin>>n>>m>>k;
+       e = 1;
+       for(i=1;i<k;i++){
+        e = (e*10)+1;
+       }
+       b = n-k;
+       f = e;
+       for(i=1;i<=b;i++){
+        f = f*10;
+       }
+       g = e;
+    b = m-k;
+       for(i=1;i<=b;i++){
+        g = g*10;
+       }
+       cout<<f<<" "<<g+e<<endl;
+    }
+}
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
