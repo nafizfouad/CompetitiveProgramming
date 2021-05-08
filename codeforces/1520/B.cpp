3dd374000a92b7379094f55f46c921f77cd94815
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-05-08 16:25:00 +0600

    Add solution for problem `B - Ordinary Numbers` Link: https://codeforces.com/contest/1520/problem/B Tags: brute force, math, number theory, *800 Ref: https://codeforces.com/contest/1520/submission/115688079

---
+++ codeforces/1520/B.cpp
@@ -0,0 +1,63 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,vv;
+vector<ll > ss[200005],pp;
+map<ll,ll > ta,pa,sa,ma;
+vector<pair < pair<ll,ll>,ll> > me,mem;
+map<string,ll>se;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<ll> li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    cin>>t;
+    p = 0;
+    while(t--)
+    {
+        v.clear();
+        cin>>n;
+        c = 0;
+        while(n){
+                v.pb(n%10);
+            n = n/10;
+            c++;
+        }
+        c = c*9-(10-(v[v.size()-1]));
+        d = 0;
+        reverse(v.begin(),v.end());
+        for(i=0;i<v.size();i++){
+            if(v[i]>v[0])break;
+            else if(v[i]<v[0]){d=1;break;}
+        }
+        if(d==0)c++;
+        cout<<c<<endl;
+    }
+}
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
