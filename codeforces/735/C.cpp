2d9b422d0097b48d455cd53a7ba5e4f6d326c454
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-05-18 01:52:00 +0600

    Add solution for problem `C - Tennis Championship` Link: https://codeforces.com/contest/735/problem/C Tags: combinatorics, constructive algorithms, greedy, math, *1600 Ref: https://codeforces.com/contest/735/submission/116515028

---
+++ codeforces/735/C.cpp
@@ -0,0 +1,49 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v[200005],s,vv,hh;
+vector<ll> ss,pp;
+map<ll,ll > ta,pa,sa,ma;
+map<pair<ll,ll>, ll >se,me;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<ll> li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>n;
+        a = 1,b=1,c=2,d=0;
+        while(c<=n)a=b,b=c,c=a+b,d++;
+        cout<<d<<endl;
+    }
+    ///1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+}
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
