46c3c107df4fa92c721356da569715242482b24c
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-05-19 02:12:00 +0600

    Add solution for problem `A - Sorting Railway Cars` Link: https://codeforces.com/contest/605/problem/A Tags: constructive algorithms, greedy, *1600 Ref: https://codeforces.com/contest/605/submission/116601346

---
+++ codeforces/605/A.cpp
@@ -0,0 +1,54 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,vv,hh;
+vector<ll> ss,pp;
+map<ll,ll > ta,pa,sa,ma;
+map<pair<ll,ll>, ll >se,me;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<ll> li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+ll ar[100005];
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>n;
+        
+        c = 0;
+        for(i=0; i<n; i++)
+        {
+            cin>>a;
+            ar[a] = 1+ar[a-1];
+            c = max(c,ar[a]);
+        }
+        cout<<n-c<<endl;
+    }
+}
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
