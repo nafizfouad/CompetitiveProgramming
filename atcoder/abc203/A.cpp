e5968f35c91d6423a3a569e4f355048dcb5325e8
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-05-30 18:01:00 +0600

    Add solution for problem `A - Chinchirorin` Link: https://atcoder.jp/contests/abc203/tasks/abc203_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc203/submissions/23034732

---
+++ atcoder/abc203/A.cpp
@@ -0,0 +1,51 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<iostream>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+//vector<ll> v,s,vv,hh;
+//vector< pair<pair<ll,ll>, ll> > ss,pp;
+//map<ll,ll > ta,pa,sa,ma;
+//map<ll,char >se,me;
+//priority_queue<ll > aq,tq,pq,nq,mq,xq;
+//string st,et,ft,pt,ct;
+//deque<ll> de,ne;
+//multiset<ll> li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>n>>m>>r;
+        if(n==m)cout<<r<<endl;
+        else if(n==r)cout<<m<<endl;
+        else if(m==r)cout<<n<<endl;
+        else cout<<0<<endl;
+    }
+}
+
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
