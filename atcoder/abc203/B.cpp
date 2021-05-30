6053d571a10f6c40ee70531d57f96478b54c6007
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-05-30 18:03:00 +0600

    Add solution for problem `B - AtCoder Condominium` Link: https://atcoder.jp/contests/abc203/tasks/abc203_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc203/submissions/23040259

---
+++ atcoder/abc203/B.cpp
@@ -0,0 +1,55 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<iostream>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+//vector<ll> v,s,vv,hh;
+//vector< pair<pair<ll,ll>, ll> > ss,pp;
+//map<ll,ll > ta,pa,sa,ma;
+//map<ll,char >se,me;
+//priority_queue<ll > aq,tq,pq,nq,mq,xq;
+//string st,et,ft,pt,ct;
+//deque<ll> de,ne;
+//multiset<ll> li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>n>>m;
+        e = 0;
+        for(i=1;i<=n;i++){
+            c = i*100;
+            for(j=1;j<=m;j++){
+                e = e+c+j;
+            }
+        }
+        cout<<e<<endl;
+    }
+}
+
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
