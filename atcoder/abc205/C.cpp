16ddfda93cc9b23545a98bc2000c1abf3157ec05
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-06-13 18:12:00 +0600

    Add solution for problem `C - POW` Link: https://atcoder.jp/contests/abc205/tasks/abc205_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc205/submissions/23415099

---
+++ atcoder/abc205/C.cpp
@@ -0,0 +1,73 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<algorithm>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,vv,hh;
+vector<string> ss,pp;
+map<ll,ll > ta,pa,sa,ma;
+map<ll,char >se,me;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<pair<ll,ll> > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>a>>b>>c;
+        if(a>0 && b>0)
+        {
+            if(a>b)cout<<">"<<endl;
+            else if(a<b)cout<<"<"<<endl;
+            else cout<<"="<<endl;
+        }
+        else {
+            if(c%2==0){
+                a = abs(a);
+                b = abs(b);
+                if(a>b)cout<<">"<<endl;
+            else if(a<b)cout<<"<"<<endl;
+            else cout<<"="<<endl;
+            }
+            else{
+              if(a>b)cout<<">"<<endl;
+            else if(a<b)cout<<"<"<<endl;
+            else cout<<"="<<endl;
+            }
+        }
+    }
+}
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
