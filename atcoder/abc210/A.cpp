cff2476e95aac3ac6f49e281e9971fe318326b27
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-07-17 18:04:00 +0600

    Add solution for problem `A - Cabbages` Link: https://atcoder.jp/contests/abc210/tasks/abc210_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc210/submissions/24282302

---
+++ atcoder/abc210/A.cpp
@@ -0,0 +1,63 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<algorithm>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,vv,hh,rr,heh;
+vector<pair<ll,ll> > ss,pp;
+map<ll,ll > ta,pa,sa,ma;
+map<ll,char >se,me;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<pair<ll,ll> > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>n>>a>>x>>y;
+        if(n<=a){
+            c = n*x;
+        }
+        else{
+                c = c+(a*x);
+                n = n-a;
+                c = c+(n*y);
+
+        }
+        cout<<c<<endl;
+    }
+}
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
