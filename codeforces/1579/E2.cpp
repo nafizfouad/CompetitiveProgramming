e3b74ac16b0ea4988dd78b79a9830a6fb51544f7
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-09-29 18:37:00 +0600

    Add solution for problem `E2 - Array Optimization by Deque` Link: https://codeforces.com/contest/1579/problem/E2 Tags: data structures, greedy, *1700 Ref: https://codeforces.com/contest/1579/submission/130264176

---
+++ codeforces/1579/E2.cpp
@@ -0,0 +1,77 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define MAXN 32000
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll > s,vv,v,hh;
+vector<pair<ll,ll> > ss,pp;
+map<ll,ll > ta,pa,sa,ma;
+map<pair<ll,ll>, ll >se,me;
+priority_queue<pair<ll,ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+multiset<ll> si,li;
+
+newset sii;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        for(i=1; i<=n; i++)
+        {
+            cin>>a;
+            v.pb(a);
+        }
+        de.pb(v[0]);
+        sii.insert(v[0]);
+        l =0;
+        for(i=1;i<n;i++){
+            a = v[i];
+
+            d = sii.order_of_key(a);
+
+            e = sii.order_of_key(a+1);
+            e = (de.size())-e;
+           // cout<<d<<" "<<e<<endl;
+            if(d<e)de.pf(a),l = l+d;
+            else de.pb(a),l = l+e;
+            sii.insert(a);
+        }
+        cout<<l<<endl;
+        de.clear();
+        v.clear();
+        sii.clear();
+    }
+
+}
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
