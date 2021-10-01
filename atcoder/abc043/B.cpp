c862f039d6f2440850293d1bc23195e30809d044
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-10-02 02:36:00 +0600

    Add solution for problem `B - Unhappy Hacking (ABC Edit)` Link: https://atcoder.jp/contests/abc043/tasks/abc043_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc043/submissions/26260482

---
+++ atcoder/abc043/B.cpp
@@ -0,0 +1,63 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<algorithm>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,heh;
+vector< pair<ll,ll> > ss,pp,hh,rr;
+map<ll,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+map<ll,vector<ll> >he;
+priority_queue<ll,vector<ll>,greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<char> de,ne;
+multiset<ll > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>st;
+        for(i=0;i<st.size();i++){
+            if(st[i]=='B'){
+                if(de.size()>0)de.pop_back();
+            }
+            else de.pb(st[i]);
+        }
+        for(i=0;i<de.size();i++)cout<<de[i];
+        cout<<endl;
+    }
+}
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
