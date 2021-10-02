9402d96736410f636e22b41d02e77ab6733afb0b
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-10-02 18:16:00 +0600

    Add solution for problem `B - typo` Link: https://atcoder.jp/contests/abc221/tasks/abc221_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc221/submissions/26290258

---
+++ atcoder/abc221/B.cpp
@@ -0,0 +1,67 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<algorithm>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,heh;
+vector< pair<ll,ll> > ss,pp,hh,rr;
+map<ll,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+map<ll,vector<ll> >he;
+priority_queue<ll,vector<ll>,greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<char> de,ne;
+multiset<ll > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>st;
+        cin>>et;
+        c = 0;
+        d = 0;
+        for(i=0; i<st.size(); i++)
+        {
+            if(st[i]!=et[i])v.pb(i);
+        }
+        for(i=0;i+1<v.size();i++){
+            if(v[i]==v[i+1]-1){swap(st[v[i]],st[v[i+1]]);break;}
+        }
+        if(st==et)cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
+}
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
