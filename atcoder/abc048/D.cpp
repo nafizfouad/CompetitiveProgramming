30cd8811fb245eea2257f72411ebda951d0946af
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-10-22 22:29:00 +0600

    Add solution for problem `D - An Ordinary Game` Link: https://atcoder.jp/contests/abc048/tasks/arc064_b Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc048/submissions/26737351

---
+++ atcoder/abc048/D.cpp
@@ -0,0 +1,64 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<queue>
+#include<set>
+#include<algorithm>
+#include<map>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define MAXN 32000
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll > s,vv,v,hh;
+vector<pair<ll,ll> > ss,pp;
+map<ll,ll > ta,pa,sa,ma;
+map<pair<ll,ll>, ll >se,me;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+multiset<ll> si,li;
+
+newset sii;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+   // cin>>t;
+    while(t--)
+    {
+        cin>>st;
+        n = st.size();
+        if(st[0]==st[n-1])c=n-3;
+        else c=n-2;
+        if(c%2==0)cout<<"Second"<<endl;
+        else cout<<"First"<<endl;
+    }
+
+}
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
