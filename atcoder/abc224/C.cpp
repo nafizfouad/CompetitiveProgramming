80c6df64838c27b0b3b54bf01ddd67c42836cb89
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-10-23 18:24:00 +0600

    Add solution for problem `C - Triangle?` Link: https://atcoder.jp/contests/abc224/tasks/abc224_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc224/submissions/26766689

---
+++ atcoder/abc224/C.cpp
@@ -0,0 +1,73 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<algorithm>
+#include<math.h>
+#include<list>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,vv[200005];
+vector<pair<ll,ll> > ss,pp,hh,rr;
+map<ll,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+map<ll,vector<ll> >he;
+priority_queue<ll,vector<ll>,greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<char> de,ne;
+multiset<ll > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>n;
+        for(i=1;i<=n;i++){
+            cin>>a>>b;
+            ss.pb({a,b});
+        }
+        f = 0;
+        for(i=0;i<n;i++){
+           for(j=i+1;j<n;j++){
+            for(k=j+1;k<n;k++){
+                a = ss[i].first * (ss[j].second - ss[k].second);
+                a = a+ ss[j].first * (ss[k].second - ss[i].second);
+                a = a+ ss[k].first * (ss[i].second - ss[j].second);
+                if(a!=0)f++;
+            }
+           }
+        }
+        cout<<f<<endl;
+    }
+}
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
