fa07eb9e42a27a7c3dac2b9d792bb2433066cca5
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-10-30 18:23:00 +0600

    Add solution for problem `C - Calendar Validator` Link: https://atcoder.jp/contests/abc225/tasks/abc225_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc225/submissions/26905032

---
+++ atcoder/abc225/C.cpp
@@ -0,0 +1,116 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<algorithm>
+#include<math.h>
+#include<list>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,vv[200005];
+vector<pair<ll,ll> > ss,pp,hh,rr;
+map<ll,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+map<ll,vector<ll> >he;
+priority_queue<ll,vector<ll>,greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<char> de,ne;
+multiset<ll > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+ll bs(ll n, ll x)
+{
+
+    ll a,b,c=1,d = n-2, an = 1e18, su = 0;
+
+    while(d>1)
+    {
+
+        if(abs(v[c]+v[d]-x)<an)
+        {
+            an = abs(v[c]+v[d]-x);
+            a = c;
+            b = d;
+        }
+        if(v[c]+v[d]>x)
+        {
+            d--;
+        }
+        else
+        {
+            c++;
+        }
+        return an;
+    }
+
+
+}
+
+int main()
+{
+    fast;
+    // cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>n>>m;
+        ll ar[n+2][m+2];
+        for(i=1; i<=n; i++)
+        {
+            for(j=1; j<=m; j++)cin>>ar[i][j];
+        }
+        for(i=1; i<=n; i++)
+        {
+            for(j=1; j<m; j++)
+            {
+                if(ar[i][j]!=ar[i][j+1]-1)
+                {
+                    f=1;
+                    break;
+                }
+                if(ar[i][j]%7==0){f=1;break;}
+            }
+        }
+        for(i=1; i<=m; i++)
+        {
+            for(j=1; j<n; j++)
+            {
+                if(ar[j][i]!=ar[j+1][i]-7)
+                {
+                    f=1;
+                    break;
+                }
+                
+            }
+        }
+        if(f==0)cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
+}
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
