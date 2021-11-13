e994796c71277083ad4c0af8a87a8aef007d0c22
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-11-13 18:13:00 +0600

    Add solution for problem `B - KEYENCE building` Link: https://atcoder.jp/contests/abc227/tasks/abc227_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc227/submissions/27216370

---
+++ atcoder/abc227/B.cpp
@@ -0,0 +1,74 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<algorithm>
+#include<math.h>
+#include<list>
+#include<iterator>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,vv,hh;
+vector<string > ss,pp,rr;
+map<ll,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+map<ll,vector<ll> >he;
+priority_queue<ll,vector<ll>,greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<char> de,ne;
+multiset<ll > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    // cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>n;
+        for(i=0;i<n;i++){
+            cin>>a;
+            v.pb(a);
+        }
+        c =0;
+        for(i=0;i<n;i++){
+            a = v[i];
+            for(j=1;j<=1050;j++){
+                for(k=1;k<=1050;k++){
+                    b = 4*j*k+3*j+3*k;
+                    if(b==a){c++;break;}
+                }
+                if(b==a)break;
+            }
+        }
+        cout<<n-c<<endl;
+    }
+}
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
