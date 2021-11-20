99f99795714d8cfc5ca001f2b8b32f2e2cb1848b
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2021-11-20 19:07:00 +0600

    Add solution for problem `A - On and Off` Link: https://atcoder.jp/contests/abc228/tasks/abc228_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc228/submissions/27387192

---
+++ atcoder/abc228/A.cpp
@@ -0,0 +1,68 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<algorithm>
+#include<math.h>
+#include<list>
+#include<iterator>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,vv,hh;
+vector<string > ss,pp,rr;
+map<ll,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+map<ll,vector<ll> >he;
+priority_queue<ll,vector<ll>,greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<char> de,ne;
+multiset<ll > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>n>>m>>k;
+        b = n;
+        f = 0;
+        for(i=0;i<24;i++){
+           if(b==k)f=1;
+           b++;
+           if(b==24)b=0;
+           if(b==m)break;
+        }
+        if(f==1)cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
+}
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
