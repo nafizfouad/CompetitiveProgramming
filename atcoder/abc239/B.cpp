562b315285b275495aedb9a6cfa5cb371327317a
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2022-02-19 18:15:00 +0600

    Add solution for problem `B - Integer Division` Link: https://atcoder.jp/contests/abc239/tasks/abc239_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc239/submissions/29442495

---
+++ atcoder/abc239/B.cpp
@@ -0,0 +1,74 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include<iostream>
+#include<vector>
+#include<map>
+#include<functional>
+#include<string>
+#include<queue>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<algorithm>
+#include<math.h>
+#include<list>
+#include<iterator>
+#include<cstring>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,vv[200005],hh;
+vector<pair<ll,ll> > ss,pp,rr;
+map<ll,ll > ta,pa,sa,ma;
+map<string,ll >se,me;
+map<ll,vector<ll> >he;
+priority_queue<ll,vector<ll>,greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<char> de,ne;
+multiset<ll > li,si;
+newset see;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    while(t--)
+    {
+      cin>>n;
+      a = n/10;
+      if(n>-10 && n<0)cout<<-1<<endl;
+      else if(a>=0){
+        cout<<a<<endl;
+      }
+      else{
+        if(n%10!=0)a--;
+        cout<<a<<endl;
+      }
+    }
+}
+
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
