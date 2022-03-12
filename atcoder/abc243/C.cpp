2ba58eca07adb42bc4cf0a61a14c3c4ddebda071
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2022-03-12 18:23:00 +0600

    Add solution for problem `C - Collision 2` Link: https://atcoder.jp/contests/abc243/tasks/abc243_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc243/submissions/30045558

---
+++ atcoder/abc243/C.cpp
@@ -0,0 +1,88 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<algorithm>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,heh;
+vector< pair<ll,ll> > ss,pp,hh,rr;
+map<ll,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+map<ll,vector<ll> >he;
+priority_queue<ll,vector<ll>,greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+multiset<ll > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>n;
+        for(i=0; i<n; i++)
+        {
+            cin>>a>>b;
+            me[ {b,a}] = i;
+        }
+        cin>>st;
+        c=1;
+        for(auto it = me.begin(); it!=me.end(); it++)
+        {
+
+            a = (*it).first.first;
+            ma[a]++;
+            if(ma.size()>c)
+            {
+                c = ma.size();
+                if(is_sorted(et.begin(),et.end())) {}
+                else
+                {
+                    f=1;
+                    break;
+                }
+                et.clear();
+            }
+            et.pb(st[(*it).second]);
+        }
+        if(is_sorted(et.begin(),et.end())) {}
+        else
+        {
+            f=1;
+        }
+        if(f==1)cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+
+    }
+}
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
