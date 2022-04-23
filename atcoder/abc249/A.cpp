8b25b4a5517a307f7e0aa8a1ffad95061d6f7ef2
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2022-04-23 18:26:00 +0600

    Add solution for problem `A - Jogging` Link: https://atcoder.jp/contests/abc249/tasks/abc249_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc249/submissions/31189490

---
+++ atcoder/abc249/A.cpp
@@ -0,0 +1,82 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<algorithm>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less_equal<ll >, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,heh;
+vector< pair<ll,ll> > ss,pp,hh,rr;
+map<char,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+map<ll,vector<ll> >he;
+priority_queue<ll,vector<ll>,greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<ll > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>a>>b>>c>>d>>e>>f>>x;
+
+        l = x/(a+c);
+        m = l;
+        l = l*b*a;
+        u = x-(m*(a+c));
+        l = l+(min(u,a)*b);
+
+        p = l;
+        swap(a,d),swap(b,e),swap(c,f);
+
+         l = x/(a+c);
+         m = l;
+        l = l*b*a;
+        u = x-(m*(a+c));
+        l = l+(min(u,a)*b);
+
+
+        //cout<<p<<" "<<l<<endl;
+        if(p==l)cout<<"Draw"<<endl;
+        else if(p>l)cout<<"Takahashi"<<endl;
+        else cout<<"Aoki"<<endl;
+    }
+}
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
