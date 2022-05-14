bd702c5a73600c4384919eef03d13d05322244e0
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2022-05-14 18:02:00 +0600

    Add solution for problem `A - Six Characters` Link: https://atcoder.jp/contests/abc251/tasks/abc251_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc251/submissions/31657664

---
+++ atcoder/abc251/A.cpp
@@ -0,0 +1,70 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<algorithm>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less_equal<ll >, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,heh;
+vector< pair<ll,ll> > ss,pp,hh,rr;
+map<ll,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+map<ll,vector<ll> >he;
+priority_queue<ll,vector<ll>,greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<ll > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>st;
+        if(st.size()==1){
+            cout<<st<<st<<st<<st<<st<<st<<endl;
+        }
+        else if(st.size()==2){
+            cout<<st<<st<<st<<endl;
+        }
+        else cout<<st<<st<<endl;
+    }
+
+}
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
