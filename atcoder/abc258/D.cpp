4923d83c9f784a32ef090a73f942747531b93d05
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2022-07-02 18:58:00 +0600

    Add solution for problem `D - Trophy` Link: https://atcoder.jp/contests/abc258/tasks/abc258_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc258/submissions/32917182

---
+++ atcoder/abc258/D.cpp
@@ -0,0 +1,82 @@
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<algorithm>
+#define ll unsigned long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less_equal<ll >, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<string> v,s,heh;
+vector< pair<ll,ll> > ss,pp,hh,rr;
+map<ll,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+map<ll,vector<ll> >he;
+priority_queue<ll,vector<ll>,greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<ll > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+    //cin>>t;
+    p = 0;
+    while(t--)
+    {
+        cin>>n>>m;
+
+        for(i=1; i<=n; i++)
+        {
+         cin>>a>>b;
+         ss.pb({a,b});
+        }
+        c = 0;
+        d = 0;
+        l = 1e19;
+        for(i=0;i<n;i++){
+            a = ss[i].first;
+            b = ss[i].second;
+            c = i+1;
+            d = d+a+b;
+            if(c>m)break;
+            f = m-c;
+            g = d+(f*b);
+            l = min(g,l);
+        }
+        cout<<l<<endl;
+    }
+
+}
+
+
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;;;;;;; ;;;;;    ;;;;;;;; ;;;;;
+;;    ;; ;;       ;;    ;; ;;
+;;    ;; ;;;;;;;; ;;    ;; ;;;;;;;;
