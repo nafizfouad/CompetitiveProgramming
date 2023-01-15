e9331e224169def670372aae7785521d78b41259
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-01-15 18:20:00 +0600

    Add solution for problem `A - Parallel Projection` Link: https://codeforces.com/contest/1782/problem/A Tags: geometry, greedy, math, *800 Ref: https://codeforces.com/contest/1782/submission/189313388

---
+++ codeforces/1782/A.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+//#define endl '\n'
+#define MAXN 200006
+#define mod 1000000007
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC tget("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,z,r,l,x,u,o,w,g,h;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll > s,v,vv,rr;
+vector<vector<ll> > pp,ss,oo,uu;
+map<ll,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+priority_queue<pair<ll,ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+vector<pair<pair<ll,ll>,ll> > de,ne;
+set<ll >si,li;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>w>>d>>h;
+        cin>>a>>b>>f>>g;
+        e = abs(a-f)+b+h+g;
+        l = a+h+f+abs(b-g);
+        r = w-a+h+w-f+abs(b-g);
+        p = d-b+d-g+h+abs(a-f);
+        e = min(l,e);
+        r = min(r,p);
+        cout<<min(e,r)<<endl;
+    }
+}
