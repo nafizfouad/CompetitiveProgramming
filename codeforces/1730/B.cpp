06c37f3e6fb87f2a270177d9a05c9d1a81550630
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-01-20 22:37:00 +0600

    Add solution for problem `B - Meeting on the Line` Link: https://codeforces.com/contest/1730/problem/B Tags: binary search, geometry, greedy, implementation, math, ternary search, *1600 Ref: https://codeforces.com/contest/1730/submission/189864338

---
+++ codeforces/1730/B.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+//#define endl '\n'
+#define MAXN 32000
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll >, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s,vv;
+vector< pair<ll,ll> > ss,pp,hh;
+map<ll,ll > ta,pa,sa,ma;
+map< pair<ll,ll>, ll >se,me;
+priority_queue<ll,vector<ll>,greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<pair<ll,ll> > de,ne;
+set<ll > li,si;
+
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+
+        for(i=0; i<n; i++)
+        {
+            cin>>a;
+            v.pb(a);
+        }
+        for(i=0; i<n; i++)
+        {
+            cin>>a;
+            s.pb(v[i]-a);
+            s.pb(v[i]+a);
+        }
+        a = s[0];
+        b = s[0];
+
+        for(i=0; i<2*n; i++)
+        {
+            a = max(a,s[i]);
+            b = min(b,s[i]);
+        }
+        //cout<<a<<" "<<b<<endl;
+        cout<<(a+b)/2;
+
+        if((a+b)%2==1)cout<<".5";
+        cout<<endl;
+
+        v.clear();
+        s.clear();
+    }
+
+}
+
