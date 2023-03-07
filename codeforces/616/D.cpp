bd3b961c056f1b38db4623e8b1a88899bc1529b3
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-03-07 19:37:00 +0600

    Add solution for problem `D - Longest k-Good Segment` Link: https://codeforces.com/contest/616/problem/D Tags: binary search, data structures, two pointers, *1600 Ref: https://codeforces.com/contest/616/submission/196389848

---
+++ codeforces/616/D.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+//#define endl '\n'
+#define MAXN 32000
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll >, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> v,s;
+vector< pair< pair<ll,ll>,pair<ll,ll> > > ss,pp,vv,hh;
+map<ll,ll > ta,pa,sa,ma;
+map< pair<pair<ll,ll>, ll>,ll >se,me;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<ll > mi,si,li;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+
+    //cin>>t;
+
+    while(t--)
+    {
+        cin>>n>>k;
+        c = 0;
+        for(i=0; i<n; i++)
+        {
+            cin>>a;
+            v.pb(a);
+        }
+        j = 0;
+        p = 0;
+        for(i=0;i<n;i++){
+            ma[v[i]]++;
+            si.insert(v[i]);
+            if(si.size()>k){
+                    if(i-j>p){
+                      p = max(p,i-j);
+                      u = j+1;
+                      o = i;
+                    }
+                    for(l=j;l<i;l++){
+                        ma[v[l]]--;
+                        if(ma[v[l]]==0)si.erase(v[l]);
+                        if(si.size()==k){j=l+1;break;}
+                    }
+
+            }
+        }
+
+        if(i-j>p){
+                      p = max(p,i-j);
+                      u = j+1;
+                      o = i;
+                    }
+        cout<<u<<" "<<o<<endl;
+    }
+}
