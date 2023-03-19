0e45a385647b663bc6c9bce059a9687ac8b217ef
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-03-19 18:05:00 +0600

    Add solution for problem `B - ASCII Art` Link: https://atcoder.jp/contests/abc294/tasks/abc294_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc294/submissions/39847228

---
+++ atcoder/abc294/B.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+//#define endl '\n'
+#define MAXN 200006
+#define mod 998244353
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,z,h,r,l,x,u,o,w,g;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<string > s,hh,vv,v;
+vector<pair<ll,ll> > pp,ss;
+map<ll,char> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+priority_queue<pair<ll,ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+multiset<ll > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+
+    //cin>>t;
+    ma[0]='.';
+    i=1;
+    for(char ch='A';ch<='Z';ch++){
+        ma[i]=ch;
+        i++;
+    }
+
+    while(t--)
+    {
+        cin>>n>>m;
+        for(i=0;i<n;i++){
+                st.clear();
+            for(j=0;j<m;j++){
+                cin>>a;
+        st.pb(ma[a]);
+            }
+            v.pb(st);
+        }
+        for(i=0;i<n;i++)cout<<v[i]<<endl;
+    }
+
+}
