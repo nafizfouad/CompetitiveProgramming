ec200c452eeafc3b6de234e18559079495e12db8
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-03-26 21:06:00 +0600

    Add solution for problem `B - Three Sevens` Link: https://codeforces.com/contest/1798/problem/B Tags: brute force, data structures, greedy, implementation, *1000 Ref: https://codeforces.com/contest/1798/submission/199268766

---
+++ codeforces/1798/B.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+//#define endl '\n'
+#define MAXN 200006
+#define mod 998244353
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,z,h,r,l,x,u,o,w,g;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll > s,hh,vv,v;
+vector<vector<ll> > pp,ss;
+map<ll,ll > ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+priority_queue<pair<ll,ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<ll > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>m;
+        for(i=0; i<m; i++)
+        {
+            cin>>n;
+            for(j=0; j<n; j++)
+            {
+                cin>>a;
+                v.pb(a);
+                ma[a]=i;
+            }
+            ss.pb(v);
+            v.clear();
+        }
+
+        for(i=0;i<m;i++){
+            for(j=0;j<ss[i].size();j++){
+                if(ma[ss[i][j]]==i){v.pb(ss[i][j]);break;}
+            }
+        }
+        if(v.size()<m)cout<<-1<<endl;
+        else {
+            for(i=0;i<m;i++)cout<<v[i]<<" ";
+            cout<<endl;
+        }
+        v.clear();
+        ss.clear();
+    }
+
+}
