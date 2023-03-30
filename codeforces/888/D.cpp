c2ca3c6ce0f018f467cf9d33963fd652612dbfe3
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-03-31 00:18:00 +0600

    Add solution for problem `D - Almost Identity Permutations` Link: https://codeforces.com/contest/888/problem/D Tags: combinatorics, dp, math, *1600 Ref: https://codeforces.com/contest/888/submission/199869709

---
+++ codeforces/888/D.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+//#define endl '\n'
+#define MAXN 200006
+#define mod 998244353
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,z,h,r,l,x,u,o,w,g;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll > s,hh,vv,v;
+vector<vector<ll> > pp,ss;
+map<ll,ll > ta,pa,sa,ma;
+map<ll,priority_queue<ll,vector<ll>,greater<ll> > >se,me;
+priority_queue<ll, vector<ll>, greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<ll > li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+
+    //cin>>t;
+
+    while(t--)
+    {
+        cin>>n>>m;
+        ma[4]=9,ma[3]=2,ma[2]=1;
+        if(n==4 && m==4){cout<<24<<endl;continue;}
+        for(i=m;i>=0;i--){
+            c = 1;
+            d = 1;
+            for(j=n,k=1;k<=i;k++,j--)c=c*j;
+            for(j=1,k=1;k<=i;k++,j++)d=d*j;
+            e = c/d;
+            f = f+(e*ma[i]);
+        }
+        cout<<f+1<<endl;
+    }
+
+}
