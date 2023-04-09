e51f6399bd9b1508399ff739184e59079f5762d9
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-04-09 22:01:00 +0600

    Add solution for problem `B - Grid Reconstruction` Link: https://codeforces.com/contest/1816/problem/B Tags: constructive algorithms, greedy, *1000 Ref: https://codeforces.com/contest/1816/submission/201538426

---
+++ codeforces/1816/B.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+//#define endl '\n'
+#define MAXN 200006
+#define mod 998244353
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,z,h,r,l,x,u,o,w,g;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll > s[130],hh,vv,v;
+vector< pair<ll,ll> > pp,ss;
+map<ll,ll > ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+priority_queue<ll, vector<ll>, greater<ll> > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<ll> li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+ll ar[10][100005];
+
+int main()
+{
+    fast;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+        for(i=1,m=n*2-1;i<n;i=i+2,m=m-2){
+            ar[1][i]=m;
+        }
+        for(i=2,m=n*2-2;i<n;i=i+2,m=m-2){
+            ar[2][i]=m;
+        }
+        ar[2][n]=2*n;
+        for(i=n,m=1;i>=1;i=i-2,m=m+2){
+            ar[1][i]=m;
+        }
+        for(i=n-1,m=2;i>=1;i=i-2,m=m+2){
+            ar[2][i]=m;
+        }
+        for(i=1;i<=2;i++){
+            for(j=1;j<=n;j++){
+                cout<<ar[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+
+    }
+
+}
