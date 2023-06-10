a4a198850a81c2daa0e65960a4e2a361ae99e6b0
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-06-10 18:08:00 +0600

    Add solution for problem `B - ABCDEFG` Link: https://atcoder.jp/contests/abc305/tasks/abc305_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc305/submissions/42129104

---
+++ atcoder/abc305/B.cpp
@@ -0,0 +1,61 @@
+/// 月よ、今こそ月明かりとなって落ちて、俺の周りに落ち続けて、それから私はあなたのものになる
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+//#define endl '\n'
+#define MAXN 2000
+#define mod 1000000007
+#define cl clear()
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC tget("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,c,n,t=1,m,p,q,d,e,f,y,z,r,l,x,u,o,w,g,h;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll > s,v,vv[100005],rr;
+vector<pair<ll,ll> > ss,oo,uu;
+vector<pair<ll,ll> > pp;
+map<ll,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll > de,ne;
+set<ll>si,li;
+
+//freopen("pow.in", "r", stdin);
+//freopen("pow.out", "w", stdout);
+
+int main()
+{
+    fast;
+
+    //cin>>t;
+
+    while(t--)
+    {
+        char ch,bh;
+        for(i=0;i<=64;i++)v.pb(0);
+        v.pb(3);
+        v.pb(1);
+        v.pb(4);
+        v.pb(1);
+        v.pb(5);
+        v.pb(9);
+        for(i=66;i<71;i++)v[i]=v[i]+v[i-1];
+        cin>>ch>>bh;
+        b = abs(ch-bh);
+        c = max(ch,bh);
+        d = min(ch,bh);
+       // cout<<c<<" "<<d<<endl;
+        cout<<v[c-1]-v[d-1]<<endl;
+    }
+}
