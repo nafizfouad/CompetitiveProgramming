e17002602ecba923c1493f08f14235dee89122e8
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-08-12 18:01:00 +0600

    Add solution for problem `A - 3.14` Link: https://atcoder.jp/contests/abc314/tasks/abc314_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc314/submissions/44488432

---
+++ atcoder/abc314/A.cpp
@@ -0,0 +1,51 @@
+/// 月よ、今こそ月明かりとなって落ちて、俺の周りに落ち続けて、それから私はあなたのものになる
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define cl clear()
+#define mod 1000000007
+//#define endl '\n'
+#define MAXN 32000
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,c,k,b,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o,w;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> s,hh,vv,v;
+vector<pair<ll,pair<ll,ll> > > ss,pp;
+map<ll,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,ll >se,me;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+priority_queue<ll> de,ne;
+set<ll> li,si;
+
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast;
+
+    //cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+        st="1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+        cout<<"3.";
+        for(i=0;i<n;i++)cout<<st[i];
+        cout<<endl;
+    }
+
+}
