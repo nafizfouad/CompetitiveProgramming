e380e2652bc18da266229dc749971755fa029c18
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-09-14 05:04:00 +0600

    Add solution for problem `D - Multiplication Table` Link: https://codeforces.com/contest/448/problem/D Tags: binary search, brute force, *1800 Ref: https://codeforces.com/contest/448/submission/223281621

---
+++ codeforces/448/D.cpp
@@ -0,0 +1,65 @@
+/// 月よ、今こそ月明かりとなって落ちて、俺の周りに落ち続けて、それから私はあなたのものになる
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define mod 1000000007
+#define mod1 1000000009
+#define cl clear()
+#define con continue
+//#define endl '\n'
+#define MAXN 200003
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,c,k,b,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o,w;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> s,hh,vv,v;
+vector<pair<ll,ll> > ss,pp;
+map<ll,ll> ta,pa,sa,ma;
+map<pair<ll,ll>,pair<ll,ll> >se,me;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll> de,ne;
+set<ll> li,si;
+
+//    freopen("output.txt", "w", stdout);
+//    freopen("sample.in", "r", stdin);
+
+int main()
+{
+    fast;
+
+    //cin>>t;
+
+    while(t--)
+    {
+        cin>>n>>p>>k;
+        l=1;
+        r=n*p;
+        while(l<=r){
+            m = (l+r)/2;
+            c=0;
+            for(i=1;i<=n;i++){
+                c = c+min(((m-1)/i),p);
+            }
+           // cout<<c<<" "<<k<<endl;
+            if(c<k){
+                l = m+1;
+                d=m;
+            }
+            else r= m-1;
+        }
+        cout<<d<<endl;
+    }
+}
+
