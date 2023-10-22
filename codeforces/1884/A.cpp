b24a678f9c28423ad7f35612b5f0021304aacfb0
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-10-22 13:21:00 +0600

    Add solution for problem `A - Simple Design` Link: https://codeforces.com/contest/1884/problem/A Tags: brute force, greedy, math, *800 Ref: https://codeforces.com/contest/1884/submission/229155524

---
+++ codeforces/1884/A.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define mod 998244353
+#define mod1 1000000009
+#define cl clear()
+#define con continue
+//#define endl '\n'
+#define MAXN 200003
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,c,k,b,n,t=1,m,p,q,d,e,f,y,g,z,h,r,l,x,u,o,w;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> s,hh,vv,v;
+vector<pair<ll,ll> > ss,pp;
+vector<vector<ll> > ii;
+map<ll,ll > ta,pa,sa,ma;
+map<ll,vector<ll> >se,me;
+map<ll,string >mee;
+map<pair<ll,ll>,ll>mepp;
+priority_queue<pair<ll,ll>,vector<pair<ll,ll> >,greater<pair<ll,ll> > > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<pair<ll,ll> > de,ne;
+multiset<ll> li,si;
+
+//    freopen("output.txt", "w", stdout);
+//    freopen("sample.in", "r", stdin);
+
+int main()
+{
+    fast;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n>>m;
+        for(i=n,j=1;j<=1000;j++,i++){
+              p=0;
+        b=i;
+        while(b){
+            p = p+(b%10);
+            b = b/10;
+        }
+            if(p%m==0){
+                cout<<i<<endl;
+                break;
+            }
+        }
+    }
+}
+
