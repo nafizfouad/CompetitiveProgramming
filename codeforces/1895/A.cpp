f66828ff5a388b6f6b1943a3d84000c609772788
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-11-03 20:40:00 +0600

    Add solution for problem `A - Treasure Chest` Link: https://codeforces.com/contest/1895/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1895/submission/231138393

---
+++ codeforces/1895/A.cpp
@@ -0,0 +1,61 @@
+/// 月よ、今こそ月明かりとなって落ちて、俺の周りに落ち続けて、それから私はあなたのものになる
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define mod 1000000007
+#define mod1 1000000009
+#define cl clear()
+#define con continue
+//#define endl '\n'
+#define MAXN 200003
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,n,t=1,m,p,q,y,z,h,r,l,x,u,o,w,c,d,e,f,g;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> s,hh,v[100005],vv;
+vector<pair<ll,ll> > ss,pp,rr;
+vector<vector<ll> > ii;
+map<char,ll > ta,pa,sa,ma;
+map<ll,vector<ll> >se,me;
+map<ll,string >mee;
+map<pair<ll,ll>,ll>mepp;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<pair<ll,ll> > de,ne;
+multiset<ll > li,si;
+
+//    freopen("output.txt", "w", stdout);
+//    freopen("sample.in", "r", stdin);
+
+int main()
+{
+    fast;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>x>>y>>k;
+        if(y<=x)cout<<x<<endl;
+        else{
+            x = x+k;
+            if(x>=y){
+                cout<<y<<endl;
+            }
+            else{
+                cout<<y+y-x<<endl;
+            }
+        }
+    }
+}
