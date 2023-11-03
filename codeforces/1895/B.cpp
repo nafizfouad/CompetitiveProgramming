bb657b07f2fda01d0e24b7d574ae21a5f93889cb
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-11-03 20:44:00 +0600

    Add solution for problem `B - Points and Minimum Distance` Link: https://codeforces.com/contest/1895/problem/B Tags: greedy, math, sortings, *800 Ref: https://codeforces.com/contest/1895/submission/231144966

---
+++ codeforces/1895/B.cpp
@@ -0,0 +1,65 @@
+/// 月よ、今こそ月明かりとなって落ちて、俺の周りに落ち続けて、それから私はあなたのものになる
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define mod 1000000007
+#define mod1 1000000009
+#define cl clear()
+#define con continue
+//#define endl '\n'
+#define MAXN 200003
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,n,t=1,m,p,q,y,z,h,r,l,x,u,o,w,c,d,e,f,g;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> s,hh,v,vv;
+vector<pair<ll,ll> > ss,pp,rr;
+vector<vector<ll> > ii;
+map<char,ll > ta,pa,sa,ma;
+map<ll,vector<ll> >se,me;
+map<ll,string >mee;
+map<pair<ll,ll>,ll>mepp;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<pair<ll,ll> > de,ne;
+multiset<ll > li,si;
+
+//    freopen("output.txt", "w", stdout);
+//    freopen("sample.in", "r", stdin);
+
+int main()
+{
+    fast;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>n;
+        for(i=0;i<2*n;i++){
+            cin>>a;
+            v.pb(a);
+        }
+        sort(v.begin(),v.end());
+        for(i=0,j=2*n-1;i<j;i++,j--){
+            ss.pb({v[i],v[j]});
+        }
+        cout<<abs(ss[0].first-ss[n-1].first)+abs(ss[0].second-ss[n-1].second)<<endl;
+        v.cl;
+        for(i=0;i<n;i++){
+            cout<<ss[i].first<<" "<<ss[i].second<<endl;
+        }
+        ss.cl;
+    }
+}
