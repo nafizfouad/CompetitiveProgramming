dbd5895e8ebfed296b9c90240039488c6d1d3451
Nafiz Fouad Sarker <nafizfouad@gmail.com>
2023-11-11 18:52:00 +0600

    Add solution for problem `D - Take ABC` Link: https://atcoder.jp/contests/abc328/tasks/abc328_d Tags: AtCoder, *425 Ref: https://atcoder.jp/contests/abc328/submissions/47488163

---
+++ atcoder/abc328/D.cpp
@@ -0,0 +1,94 @@
+/// 月よ、今こそ月明かりとなって落ちて、俺の周りに落ち続けて、それから私はあなたのものになる
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define ld long double
+#define pi acos(-1)
+#define pb push_back
+#define pf push_front
+#define mod 1000000007
+#define mod1 1000000009
+#define cl clear()
+#define con continue
+//#define endl '\n'
+#define MAXN 200003
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt,tune=native")
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        newset;
+ll i,j,a,k,b,n,t=1,m,p,q,y,z,h,r,l,x,u,o,w,c,d,e,f,g;
+ld aa,bb,ee,dd,ff,kk,gg,cc;
+vector<ll> s,v,vv,rr,hh,pp;
+vector<pair<ll,ll> > ss;
+vector<vector<ll> > ii;
+map<ll,ll > ta,pa,sa,ma;
+map<ll,ll >se,me;
+map<pair<ll,ll>,ll >mee;
+map<pair<ll,ll>,ll>mepp;
+priority_queue<ll > aq,tq,pq,nq,mq,xq;
+string st,et,ft,pt,ct;
+deque<ll > de,ne;
+set<ll > li,si;
+
+//    freopen("output.txt", "w", stdout);
+//    freopen("sample.in", "r", stdin);
+
+ll ar[300006];
+
+int main()
+{
+    fast;
+    //cin>>t;
+
+    while(t--)
+    {
+        cin>>st;
+        n=st.size();
+        st="12"+st;
+        for(i=0; i<st.size(); i++)
+        {
+            si.insert(i);
+        }
+        auto ki=si.begin();
+        ++ki;
+        ++ki;
+        while(1)
+        {
+            v.cl;
+            f=0;
+            for(auto it = ki; it!=si.end(); it++)
+            {
+                c=(*it);
+                --it;
+                b=(*it);
+                --it;
+                a=(*it);
+
+                if(st[c]=='C' && st[b]=='B' && st[a]=='A')
+                {
+                    v.pb(a),v.pb(b),v.pb(c);
+                    ++it;
+                    ++it;
+                    ++it;
+                    ki=it;
+                    f=1;
+                    break;
+                }
+                else ++it,++it;
+            }
+            for(i=0;i<v.size();i++)si.erase(v[i]);
+            if(f==0)break;
+        }
+
+        for(auto x:si){
+            ft.pb(st[x]);
+        }
+        for(i=2;i<ft.size();i++)cout<<ft[i];
+        cout<<endl;
+    }
+}
